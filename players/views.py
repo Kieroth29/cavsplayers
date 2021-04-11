524dfcdb998caaebf2fdb2465a784dc3000e2350
Kieroth29 <kkinhuxd@gmail.com>
2021-04-11 19:30:59 -0300

    Position filter using get request and HTML select value

--- players/views.py
+++ players/views.py
@@ -4,4 +4,7 @@ from .models import Player
 
 def index(request):
 	playerdata = Player.objects.all()
+	posfilter = request.GET.get('pos-filter')
+	q1 = Player.objects.filter(main_position=posfilter)
+	print(q1)
 	return render(request,'index.html',locals())
